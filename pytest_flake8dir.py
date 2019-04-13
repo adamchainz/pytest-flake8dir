1b2229d00df627d3fadf813fdc49a7f9b2201011
Adam Johnson <me@adamj.eu>
2019-04-13 15:10:33 +0200

    Run flake8 in a subprocess (#68)

--- pytest_flake8dir.py
+++ pytest_flake8dir.py
@@ -1,10 +1,7 @@
-import io
-import sys
-from contextlib import contextmanager
+import subprocess
 from textwrap import dedent
 
 import pytest
-from flake8.main.cli import main as flake8_main
 
 
 __version__ = '2.0.0'
@@ -52,14 +49,15 @@ class Flake8Dir(object):
         if extra_args:
             args.extend(extra_args)
 
-        exit_code = 0  # In case --exit-zero has been passed
-        with self.tmpdir.as_cwd(), patch_sys_argv(args), captured_stdout() as stdout:
-            try:
-                flake8_main()
-            except SystemExit as exc:
-                exit_code = exc.code
-        full_output = stdout.getvalue()
-        return Flake8Result(full_output, exit_code)
+        process = subprocess.Popen(
+            args=args,
+            cwd=str(self.tmpdir),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        process.wait()
+        return Flake8Result(process.stdout.read(), process.returncode)
 
 
 class Flake8Result(object):
@@ -71,34 +69,3 @@ class Flake8Result(object):
         if lines[-1] == '':
             lines = lines[:-1]
         self.out_lines = lines
-
-
-@contextmanager
-def patch_sys_argv(new_argv):
-    orig = sys.argv
-    sys.argv = new_argv
-    yield
-    sys.argv = orig
-
-
-@contextmanager
-def captured_output(stream_name):
-    """Return a context manager used by captured_stdout/stdin/stderr
-    that temporarily replaces the sys stream *stream_name* with a StringIO.
-    Note: This function and the following ``captured_std*`` are copied
-          from CPython's ``test.support`` module."""
-    orig_stdout = getattr(sys, stream_name)
-    setattr(sys, stream_name, io.StringIO())
-    try:
-        yield getattr(sys, stream_name)
-    finally:
-        setattr(sys, stream_name, orig_stdout)
-
-
-def captured_stdout():
-    """Capture the output of sys.stdout:
-       with captured_stdout() as stdout:
-           print("hello")
-       self.assertEqual(stdout.getvalue(), "hello\n")
-    """
-    return captured_output("stdout")
