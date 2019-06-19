0437118af84e9c5fff0e08ee125581ee22b52533
Adam Johnson <me@adamj.eu>
2019-06-19 10:50:00 +0100

    Format code with Black
    * Add black and flake8-bugbear - automatically picked up by multilint
    * Add pyproject.toml black configuration to target Python 3.5
    * Reconfigure isort and flake8
    * Drop flake8-commas and rely on Black's trailing comma insertion only
    * Run black on the code
    * Add README badge

--- setup.py
+++ setup.py
@@ -4,56 +4,51 @@ from setuptools import setup
 
 
 def get_version(filename):
-    '''
+    """
     Return package version as listed in `__version__` in `filename`.
-    '''
-    with open(filename, 'r') as fp:
+    """
+    with open(filename, "r") as fp:
         init_py = fp.read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
-version = get_version('pytest_flake8dir.py')
+version = get_version("pytest_flake8dir.py")
 
 
-with open('README.rst', 'r') as readme_file:
+with open("README.rst", "r") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst', 'r') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
+with open("HISTORY.rst", "r") as history_file:
+    history = history_file.read().replace(".. :changelog:", "")
 
 
 setup(
-    name='pytest-flake8dir',
+    name="pytest-flake8dir",
     version=version,
-    description='A pytest fixture for testing flake8 plugins.',
-    long_description=readme + '\n\n' + history,
-    author='Adam Johnson',
-    author_email='me@adamj.eu',
-    url='https://github.com/adamchainz/pytest-flake8dir',
-    py_modules=['pytest_flake8dir'],
+    description="A pytest fixture for testing flake8 plugins.",
+    long_description=readme + "\n\n" + history,
+    author="Adam Johnson",
+    author_email="me@adamj.eu",
+    url="https://github.com/adamchainz/pytest-flake8dir",
+    py_modules=["pytest_flake8dir"],
     include_package_data=True,
-    install_requires=[
-        'flake8',
-        'pytest',
-    ],
-    python_requires='>=3.5',
-    license='ISC License',
+    install_requires=["flake8", "pytest"],
+    python_requires=">=3.5",
+    license="ISC License",
     zip_safe=False,
-    keywords='pytest, flake8',
-    entry_points={
-        'pytest11': ['flake8dir = pytest_flake8dir'],
-    },
+    keywords="pytest, flake8",
+    entry_points={"pytest11": ["flake8dir = pytest_flake8dir"]},
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Pytest',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
--- test_pytest_flake8dir.py
+++ test_pytest_flake8dir.py
@@ -4,12 +4,14 @@ import six
 
 
 def test_make_py_files_single(flake8dir):
-    flake8dir.make_py_files(example="""
+    flake8dir.make_py_files(
+        example="""
         x  = 1
-    """)
+    """
+    )
     result = flake8dir.run_flake8()
     assert result.out_lines == [
-        './example.py:1:2: E221 multiple spaces before operator'
+        "./example.py:1:2: E221 multiple spaces before operator"
     ]
     assert result.exit_code == 1
 
@@ -21,44 +23,53 @@ def test_make_py_files_double(flake8dir):
         """,
         example2="""
             y  = 2
-        """
+        """,
     )
     result = flake8dir.run_flake8()
     assert set(result.out_lines) == {
-        './example1.py:1:2: E221 multiple spaces before operator',
-        './example2.py:1:2: E221 multiple spaces before operator',
+        "./example1.py:1:2: E221 multiple spaces before operator",
+        "./example2.py:1:2: E221 multiple spaces before operator",
     }
 
 
 def test_make_py_files_no_positional_args(flake8dir):
     with pytest.raises(TypeError) as excinfo:
-        flake8dir.make_py_files(1, example="""
+        flake8dir.make_py_files(
+            1,
+            example="""
             x  = 1
-        """)
-    assert 'make_py_files takes no positional arguments' in six.text_type(excinfo.value)
+        """,
+        )
+    assert "make_py_files takes no positional arguments" in six.text_type(excinfo.value)
 
 
 def test_make_py_files_requires_at_least_one_kwarg(flake8dir):
     with pytest.raises(TypeError) as excinfo:
         flake8dir.make_py_files()
-    assert 'make_py_files requires at least one keyword argument' in six.text_type(excinfo.value)
+    assert "make_py_files requires at least one keyword argument" in six.text_type(
+        excinfo.value
+    )
 
 
 def test_make_example_py(flake8dir):
-    flake8dir.make_example_py("""
+    flake8dir.make_example_py(
+        """
         x  = 1
-    """)
+    """
+    )
     result = flake8dir.run_flake8()
     assert result.out_lines == [
-        './example.py:1:2: E221 multiple spaces before operator'
+        "./example.py:1:2: E221 multiple spaces before operator"
     ]
 
 
 def test_make_setup_cfg(flake8dir):
-    flake8dir.make_setup_cfg("""
+    flake8dir.make_setup_cfg(
+        """
         [flake8]
         ignore = E221
-    """)
+    """
+    )
     flake8dir.make_py_files(
         example="""
         x  = 1
@@ -69,12 +80,15 @@ def test_make_setup_cfg(flake8dir):
 
 
 def test_make_file(flake8dir):
-    flake8dir.make_file('myexample.py', """
+    flake8dir.make_file(
+        "myexample.py",
+        """
         x  = 1
-    """)
+    """,
+    )
     result = flake8dir.run_flake8()
     assert result.out_lines == [
-        './myexample.py:1:2: E221 multiple spaces before operator'
+        "./myexample.py:1:2: E221 multiple spaces before operator"
     ]
 
 
@@ -84,14 +98,14 @@ def test_extra_args(flake8dir):
         x  = 1
         """
     )
-    result = flake8dir.run_flake8(extra_args=['--ignore', 'E221'])
+    result = flake8dir.run_flake8(extra_args=["--ignore", "E221"])
     assert result.out_lines == []
 
 
 def test_extra_args_version(flake8dir):
-    result = flake8dir.run_flake8(extra_args=['--version'])
+    result = flake8dir.run_flake8(extra_args=["--version"])
     assert len(result.out_lines) == 1
-    assert result.out_lines[0].startswith(flake8.__version__ + ' ')
+    assert result.out_lines[0].startswith(flake8.__version__ + " ")
     assert result.exit_code == 0
 
 
@@ -101,4 +115,4 @@ def test_separate_tmpdir(flake8dir, tmpdir):
         x  = 1
         """
     )
-    assert not tmpdir.join('example.py').check()
+    assert not tmpdir.join("example.py").check()
